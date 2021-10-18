ed1066dd4f7fab46db50b57ceb887fd0acab5150
KaranVirupakshar <karan.virupakshar@gmail.com>
2021-10-17 21:10:21 -0400

    a5 JavaScript fa21

--- public/a5/build/PostList/PostItem.js
+++ public/a5/build/PostList/PostItem.js
@@ -11,19 +11,32 @@ const PostItem = (homepost) => {
 
    
     <div class="p-1 ms-4" > 
+    
+    <div class="row">
+    
+    <div class="col-xxl-11 col-xl-11 col-lg-11 col-md-11 col-sm-11 col-11">
           <span class="p-1 wd-fonts-bold wd-fonts-white"> ${homepost.userName} <i class="px-1 fas fa-check-circle"></i> </span>
           <span class="wd-fonts-lightgray"> ${homepost.handle}  &middot; ${homepost.time} </span>
           
+          
+          
           <div class="wd-fonts-white p-1" > ${homepost.title}</div>
+          </div>
+          
+          <div class="col-xxl-1 col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1">
+          <i class="fas fa-ellipsis-h"></i>
+          
+            </div>
+          </div>
           <div class="wd-thinrounded-corners-all-around wd-border-solid wd-border-thin wd-border-darkgray">
-            <div class="" > <img class="wd-top-rounded wd-bottom-rounded wd-border-solid wd-border-thin wd-border-darkgray" src=${homepost.image} width="100%" height="274px"/> </div>
-                    <div class="ms-2 wd-fonts-white" > <span class="wd-fonts-bold"> ${homepost.imageTitle} </span></div>
+            <div class="" > <img class="wd-top-rounded  wd-border-thin wd-border-darkgray ${homepost.handle === "@elonmusk" ? 'wd-border-solid' : 'wd-bottom-rounded'} " src=${homepost.image} width="100%" height="274px"/> </div>
+                    <div class="ms-2 wd-fonts-white ${homepost.handle === "@elonmusk" ? 'mt-2' : ''}" > <span class="wd-fonts-bold"> ${homepost.imageTitle} </span></div>
                     <div class="ms-2 me-2" > ${homepost.imageDesc} </div>
                     <div class="ms-2 mb-1 mt-1" > ${homepost.linkicon}  ${homepost.link} </div>
           </div>
         
         
-        <div class="py-3 d-flex justify-content-evenly" >
+        <div class="py-3 me-4 ms-4 d-flex justify-content-between" >
             <a class="wd-fonts-highlight" style="text-decoration: none;"> <i class="far fa-comment"></i> <span class="wd-margin-left"> ${homepost.message} </span></a>
             <a class="wd-fonts-highlight ms-4" style="text-decoration: none;"> <i class="fa fa-retweet"></i> <span class="wd-margin-left"> ${homepost.retweet} </span></a>
             <a class="wd-fonts-highlight ms-4" style="text-decoration: none;"> <span class="wd-fg-color-red"> <i class="far fa-heart"></i> </span>  <span class="wd-margin-left">${homepost.like} </span></a>
